1d7320b2ff192f691c9a47af99995f33183dc0b1
hyun1e <jasmine.kim.18@gmail.com>
2018-01-27 12:23:09 -0800

    Tree library

--- tree.py
+++ tree.py
@@ -3,6 +3,7 @@ class BinaryTreeNode:
 		self.data = data
 		self.left = left
 		self.right = right
+		#print("CREATING NODE:", self.data)
 
 	def get_left_child(self):
 		return self.left
@@ -28,44 +29,75 @@ class BinaryTreeNode:
 			tree.right = self.right
 
 def bst_insert(root, node):
+	print("INSERT")
+	print("root.data, node.data",root.data, node.data)
 	if root is None:
 		root = node
 	else:
 		if root.data > node.data:
+			#print("<<<<<<<<<<<<<")
+			#print("root.data > node.data",root.data, node.data)
+			#print("root.left", root.left)
 			if root.left is None:
 				root.left = node
 			else:
 				bst_insert(root.left, node)
 		else:
+			#print(">>>>>>>>>>>>>")
+			#print("root.data < node.data",root.data, node.data)
+			#print("root.right", root.right)
 			if root.right is None:
 				root.right = node
 			else:
 				bst_insert(root.right, node)
 
 
-def inorder_traversal(tree):
+def inorder_traversal(tree, direction):
+	print("LOOP!")
 	if tree is not None:
-		inorder_traversal(tree.get_left_child())
+		print("Direction", direction)
+		print("Tree.get_node()", tree.get_node())
+		print("Tree.get_left_child()", tree.get_left_child())
+		print("Tree.get_right_child()", tree.get_right_child())
+		print(" ")
+		postorder_count(tree.get_left_child(), "left")
 		print(tree.get_node())
-		inorder_traversal(tree.get_right_child())
+		print(" ")
+		postorder_count(tree.get_right_child(), "right")
 
 def preorder_traversal(tree):
 	if tree is not None:
 		print(tree.get_node())
-		inorder_traversal(tree.get_left_child())
-		inorder_traversal(tree.get_right_child())
+		preorder_traversal(tree.get_left_child())
+		preorder_traversal(tree.get_right_child())
 
 def postorder_traversal(tree):
 	if tree is not None:
-		inorder_traversal(tree.get_left_child())
-		inorder_traversal(tree.get_right_child())
+		postorder_traversal(tree.get_left_child())
+		postorder_traversal(tree.get_right_child())
 		print(tree.get_node())
+def create_bst(tree, keys):
+	for key in keys:
+		bst_insert(tree, BinaryTreeNode(key))
+		#inorder_traversal(tree, "root")
 
-tree = BinaryTreeNode(8)
-keys = [4, 12, 2, 6, 10, 14, 1, 3, 5, 7, 9, 11, 13, 15]
+def postorder_count(tree, direction):
+	if tree is not None:
+		#print("Direction", direction)
+		#print("Tree.get_node()", tree.get_node())
+		#print("Tree.get_left_child()", tree.get_left_child())
+		#print("Tree.get_right_child()", tree.get_right_child())
+		postorder_count(tree.get_left_child(), "left")
+		postorder_count(tree.get_right_child(), "right")
+		print(tree.get_node())
+	#return tree
 
-for key in keys:
-	bst_insert(tree, BinaryTreeNode(key))
-inorder_traversal(tree)
 
+
+tree = BinaryTreeNode(200)
+#keys = [4, 12, 2, 6, 10, 14, 1, 3, 5, 7, 9, 11, 13, 15]
+#keys = [4, 12, 2, 6]
+keys = [100,75,150,25,80,125,175,1,30,400,250,300,350,500]
+create_bst(tree, keys)
+postorder_count(tree, "root")
 
